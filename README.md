3d2c45cb35a35b72e7470bdc0e423d2e4664a9f9
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-09-07 19:33:18 -0400

    Initial commit of structure

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-turtle.io
-=========
+# turtle.io
 
-Web server built with abaaso &amp; node.js
+Webserver built on  abaaso & node.js
+
+## Getting Started
+Install the module with: `npm install turtle.io`
+
+```javascript
+var turtle_io = require('turtle.io');
+turtle_io.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Jason Mulligan  
+Licensed under the BSD-3 license.
---
+++ grunt.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg : "<json:package.json>",
+    meta : {
+          banner : "/**\n" + 
+                   " * <%= pkg.name %>\n" +
+                   " *\n" +
+                   " * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n" +
+                   " * @copyright <%= pkg.author.name %> <%= grunt.template.today('yyyy') %>\n" +
+                   " * @license <%= pkg.licenses[0].type %> <<%= pkg.licenses[0].url %>>\n" +
+                   " * @link <%= pkg.homepage %>\n" +
+                   " * @version <%= pkg.version %>\n" +
+                   " */"
+    },
+    concat: {
+      dist: {
+        src : [
+          "<banner>",
+          "src/intro.js",
+          "src/core.js",
+          "src/prototype.js",
+          "src/outro.js"
+        ],
+        dest : "lib/turtle.io.js"
+      }
+    },
+    test : {
+      files : ["test/**/*.js"]
+    },
+    watch : {
+      files : "<config:lint.files>",
+      tasks : "default"
+    },
+    jshint : {
+      options : {
+        curly   : true,
+        eqeqeq  : true,
+        immed   : true,
+        latedef : true,
+        newcap  : true,
+        noarg   : true,
+        sub     : true,
+        undef   : true,
+        boss    : true,
+        eqnull  : true,
+        node    : true
+      },
+      globals: {
+        exports : true
+      }
+    }
+  });
+
+  grunt.registerTask("default", "concat test");
+};
