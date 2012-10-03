4637b2bf876a695aa36d06fa2c3bb8f700203f66
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-10-03 12:42:53 -0400

    Generating Server header once

--- src/headers.js
+++ src/headers.js
@@ -7,7 +7,7 @@ var headers = {
 	"Accept"                       : "text/html, text/plain",
 	"Content-Type"                 : "text/html",
 	"Date"                         : "",
-	"Server"                       : "turtle.io/{{VERSION}} [abaaso/" + $.version + " node.js/" + process.versions.node.replace(/^v/, "") + " (" + process.platform.capitalize() + " V8/" + process.versions.v8 + ")]",
+	"Server"                       : (function () { return ("turtle.io/{{VERSION}} [abaaso/" + $.version + " node.js/" + process.versions.node.replace(/^v/, "") + " (" + process.platform.capitalize() + " V8/" + process.versions.v8 + ")]"); })(),
 	"Access-Control-Allow-Origin"  : "*",
 	"Access-Control-Allow-Headers" : "Accept, Allow, Cache-Control, Content-Type, Date, Etag, Transfer-Encoding, Server"
 };
