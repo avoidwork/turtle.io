b9ea2cfe774e7be30c57339f0d662fb427ba23b9
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-10-06 17:45:32 -0400

    Forgot to implement 412 status (oops)

--- src/write.js
+++ src/write.js
@@ -23,14 +23,19 @@ factory.prototype.write = function (path, res, req) {
 
 		req.on("end", function () {
 			fs.readFile(path, function (e, data) {
+				var hash = "\"" + self.hash(data) + "\"";
+
 				if (e) self.respond(res, req, messages.ERROR_APPLICATION, codes.ERROR_APPLICATION);
 				switch (true) {
 					case !req.headers.hasOwnProperty(etag):
-					case req.headers.etag === self.hash(data):
+					case req.headers.etag === hash:
 						fs.writeFile(path, body, function (e) {
 							if (e) self.respond(res, req, messages.ERROR_APPLICATION, codes.ERROR_APPLICATION);
-							else self.respond(res, req, (put ? messages.NO_CONTENT : messages.CREATED), (put ? codes.NO_CONTENT : codes.CREATED), {"Allow" : allow});
+							else self.respond(res, req, (put ? messages.NO_CONTENT : messages.CREATED), (put ? codes.NO_CONTENT : codes.CREATED), {"Allow" : allow, Etag: hash});
 						});
+						break;
+					case req.headers.etag !== hash:
+						self.respond(res, req, null, codes.FAILED);
 						break;
 					default:
 						self.respond(res, req, messages.ERROR_APPLICATION, codes.ERROR_APPLICATION);
