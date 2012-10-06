55689fe2ba82be0d4cce192022615c13c3d0f16d
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-10-06 15:57:41 -0400

    Creating log dir if it doesn't exist
    Fixed typo

--- src/log.js
+++ src/log.js
@@ -10,7 +10,17 @@ factory.prototype.log = function (msg, error, display) {
 	error   = (error   === true);
 	display = (display !== false);
 
-	var date, filename, text;
+	var err = "Could not write to msg to log",
+	    dir = this.config.logs.dir,
+	    date, filename, text, append;
+
+	// Appends text to the log file
+	append = function (filename, text) {
+		fs.appendFile((dir + "/" + filename), text, function (e) {
+			if (e) $.log(e);
+			if (REGEX_HALT.test(text)) process.exit(0);
+		});
+	};
 
 	// Displaying on the console
 	if (display) $.log(msg);
@@ -20,11 +30,13 @@ factory.prototype.log = function (msg, error, display) {
 		date     = new Date();
 		text     = moment(date).format("HH:MM:SS") + " " + msg + "\n" + (typeof msg.stack !== "undefined" ? msg.stack + "\n" : "");
 		filename = this.config.logs[error ? "error" : "daemon"].replace(/\{\{date\}\}/, moment(date).format(this.config.logs.date));
-		fs.appendFile(("./logs/" + filename), text, function (e) {
-			if (e) return $.log("Could not write to msgor log");
 
-			// Halting on fundamental msgor
-			if (REGEX_HALT.test(msg)) process.exit(0);
+		fs.exists(dir, function (exists) {
+			if (exists) append(filename, text);
+			else fs.mkdir(dir, function (e) {
+				if (e) $.log(e);
+				else append(filename, text);
+			});
 		});
 	}
 	else if (REGEX_HALT.test(msg)) process.exit(0);
