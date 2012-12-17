83eb4db419f47a2f80ae2eb95204c1edcdab1fe5
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-12-17 15:47:33 -0500

    Updating logging to utilize __dirname, changed config.logs.file to be the filename only

--- grunt.js
+++ grunt.js
@@ -53,28 +53,6 @@ module.exports = function (grunt) {
 		},
 		test : {
 			files : ["test/**/*.js"]
-		},
-		watch : {
-			files : "<config:lint.files>",
-			tasks : "default"
-		},
-		jshint : {
-			options : {
-				curly   : true,
-				eqeqeq  : true,
-				immed   : true,
-				latedef : true,
-				newcap  : true,
-				noarg   : true,
-				sub     : true,
-				undef   : true,
-				boss    : true,
-				eqnull  : true,
-				node    : true
-			},
-			globals: {
-				exports : true
-			}
 		}
 	});
 
