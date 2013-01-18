3b2809b203e62ee615d463ed0125ab32ae5e7680
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-01-18 11:47:06 -0500

    Separating `sample` from `config`

--- sample.js
+++ sample.js
@@ -1,4 +1,25 @@
 var turtle = require("./lib/turtle.io"),
-    server = new turtle();
+    server = new turtle(),
+    config;
 
-server.start();
+config = {
+	auth : {
+		test2 : {
+			authRealm : "Private",
+			authList  : ["admin:admin"]
+		}
+	},
+	root   : "./sites",
+	vhosts : {
+		"localhost" : "test",
+		"test"      : "test",
+		"test2"     : "test2",
+		"all"       : "test"
+	}
+}
+
+server.get("/status", function (res, req) {
+	server.respond(res, req, server.status());
+}, "localhost");
+
+server.start(config);
