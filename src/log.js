57bb8b182e6d2a95b0189b847ad3679acace518c
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-03-19 16:51:26 -0400

    Fixing a regression affecting writing to the log file, fixing an argument passed to a dtrace probe

--- src/log.js
+++ src/log.js
@@ -7,7 +7,7 @@
 factory.prototype.log = function ( msg ) {
 	var self = this,
 	    err  = msg.callstack !== undefined,
-	    file = self.config.logs.file.replace(" {{ext}}", new moment().format( this.config.logs.ext ) ),
+	    file = self.config.logs.file.replace("{{ext}}", new moment().format( this.config.logs.ext ) ),
 	    exit;
 
 	/**
