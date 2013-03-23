94da91b165a6d7a2dabcd471b8a6c4c315f6f954
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-03-23 06:58:02 -0400

    Fixing `route-set` DTrace probe arguments

--- trace.sh
+++ trace.sh
@@ -12,7 +12,7 @@ sudo dtrace -Z -n 'turtle-io*:::allowed{ trace(copyinstr(arg0)); trace(copyinstr
                -n 'turtle-io*:::redirect-set{ trace(copyinstr(arg0)); trace(copyinstr(arg1)); trace(copyinstr(arg2)); trace(arg3); trace(arg4); }'  \
                -n 'turtle-io*:::request{ trace(copyinstr(arg0)); trace(copyinstr(arg1)); trace(arg2); }'  \
                -n 'turtle-io*:::respond{ trace(copyinstr(arg0)); trace(copyinstr(arg1)); trace(copyinstr(arg2)); trace(arg3); trace(arg4); }'  \
-               -n 'turtle-io*:::route-set{ trace(copyinstr(arg0)); trace(copyinstr(arg1)); trace(arg2); }'  \
+               -n 'turtle-io*:::route-set{ trace(copyinstr(arg0)); trace(copyinstr(arg1)); trace(copyinstr(arg2)); trace(arg3); }'  \
                -n 'turtle-io*:::route-unset{ trace(copyinstr(arg0)); trace(copyinstr(arg1)); trace(copyinstr(arg2)); trace(arg3); }'  \
                -n 'turtle-io*:::status{ trace(arg0); trace(arg1); trace(arg2); trace(arg3); }'  \
                -n 'turtle-io*:::write{ trace(copyinstr(arg0)); trace(copyinstr(arg1)); trace(copyinstr(arg2)); trace(copyinstr(arg3)); trace(arg4); }'
