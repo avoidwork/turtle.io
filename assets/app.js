4824a60645cf4ed403052a08da5b8e64e3e30a47
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-04-27 14:47:13 -0400

    Removing version from footer

--- assets/app.js
+++ assets/app.js
@@ -4,19 +4,14 @@
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
  * @copyright Jason Mulligan 2013
  * @license BSD3 <http://opensource.org/licenses/BSD-3-Clause>
- * @link https://github.com/avoidwork/turtle.io.website
+ * @link https://github.com/avoidwork/turtle.io
  * @module turtle.io
- * @version 0.1.1
+ * @version 0.1.2
  */
 
 (function ($) {
 	$.on("ready", function () {
 		// Setting footer copyright
 		$("#year").html(new Date().getFullYear());
-
-		// Setting version of Server
-		"/".headers(function (args) {
-			$("#version").html(/turtle.io\/([\d|\.|a|b]+)/.exec(args.Server)[1]);
-		});
 	});
 })(abaaso);
