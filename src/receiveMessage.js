15c2c534898682ad4f2b42f48222c1fd96f81c9e
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-05-26 10:49:37 -0400

    Simplfied `this.start()` for master & utilizing message passing to start workers (in progress)

--- src/receiveMessage.js
+++ src/receiveMessage.js
@@ -30,6 +30,49 @@ factory.prototype.receiveMessage = function ( msg ) {
 
 			$.merge( this.sessions[msg.arg.id], $.decode( msg.arg.session ) );
 			break;
+
+		case MSG_START:
+			// Starting queue processor
+			if ( cluster.worker.id === "1" ) {
+				this.mode( true );
+			}
+			// Starting http workers
+			else {
+				// Setting error handler
+				if ( typeof fn === "function" ) {
+					error = fn;
+				}
+				else {
+					error = function ( res, req, timer ) {
+						var body   = messages.NOT_FOUND,
+						    status = codes.NOT_FOUND,
+						    method = req.method.toLowerCase(),
+						    host   = req.headers.host.replace( /:.*/, "" );
+
+						if ( !REGEX_GET.test( method ) ) {
+							if ( allowed( req.method, req.url, host ) ) {
+								body   = messages.ERROR_APPLICATION;
+								status = codes.ERROR_APPLICATION;
+							}
+							else {
+								body   = messages.NOT_ALLOWED;
+								status = codes.NOT_ALLOWED;
+							}
+						}
+
+						self.respond( res, req, body, status, {"Cache-Control": "no-cache"}, timer, false );
+					}
+				}
+
+				// Bootstrapping instance
+				self.bootstrap.call( self, error );
+			}
+
+
+
+			break;
+
+
 	}
 
 	// Acknowledging message
