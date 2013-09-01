8ac9a175073f1a9e34636d823daa9ad3efaa22c3
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-09-01 17:36:07 -0400

    Fixing wildcard hosts

--- src/host.js
+++ src/host.js
@@ -8,7 +8,7 @@
 TurtleIO.prototype.host = function ( arg ) {
 	if ( this.handlers.all.hosts[arg] === undefined ) {
 		this.vhosts.push( arg );
-		this.vhostsRegExp.push( new RegExp( "^" + arg + "$" ) );
+		this.vhostsRegExp.push( new RegExp( "^" + arg.replace( /\*/g, ".*" ) + "$" ) );
 		this.handlers.all.hosts[arg]       = {};
 		this.handlers["delete"].hosts[arg] = {};
 		this.handlers.get.hosts[arg]       = {};
