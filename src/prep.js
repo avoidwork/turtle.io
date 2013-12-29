5d682eb1cec20a5f91a3ad6dfa7b8ea674d6c33f
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-12-29 12:54:55 -0500

    Fixing a double quote of referer header in `prep()`

--- src/prep.js
+++ src/prep.js
@@ -8,10 +8,9 @@
  * @return {String}         Log message
  */
 TurtleIO.prototype.prep = function ( req, res, headers ) {
-	var msg   = this.config.logs.format,
-	    user  = req.parsed ? ( req.parsed.auth.split( ":" )[0] || "-" ) : "-",
-	    refer = req.headers.referer ? ( "\"" + req.headers.referer + "\"" ) : "-",
-	    ip    = req.headers["x-forwarded-for"] ? req.headers["x-forwarded-for"].explode().last() : req.connection.remoteAddress;
+	var msg  = this.config.logs.format,
+	    user = req.parsed ? ( req.parsed.auth.split( ":" )[0] || "-" ) : "-",
+	    ip   = req.headers["x-forwarded-for"] ? req.headers["x-forwarded-for"].explode().last() : req.connection.remoteAddress;
 
 	msg = msg.replace( "%v",             req.headers.host )
 	         .replace( "%h",             ip )
@@ -21,7 +20,7 @@ TurtleIO.prototype.prep = function ( req, res, headers ) {
 	         .replace( "%r",             req.method + " " + req.url + " HTTP/1.1" )
 	         .replace( "%>s",            res.statusCode )
 	         .replace( "%b",             headers["Content-Length"] || "-" )
-	         .replace( "%{Referer}i",    refer )
+	         .replace( "%{Referer}i",    req.headers.referer       || "-" )
 	         .replace( "%{User-agent}i", req.headers["user-agent"] || "-" );
 
 	return msg;
