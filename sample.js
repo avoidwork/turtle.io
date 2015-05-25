2bb8183170cb49ad6ad8c8518e212e6847483205
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-05-24 22:11:11 -0400

    Reverting a change to the sample server

--- sample.js
+++ sample.js
@@ -8,8 +8,6 @@ server.get("/status", function (req, res) {
 	server.respond(req, res, server.status());
 }, "test");
 
-server.proxy("/github", "https://api.github.com");
-
 server.start( {
 	default : "test",
 	root    : dir + "/sites",
