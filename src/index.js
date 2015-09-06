cd49c7835c0aa914b997989daa40aaf305d5afd0
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-09-06 10:23:07 -0400

    Adding `retsu` to `index.js`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const array = require("retsu");
 const path = require("path");
 const middleware = require(path.join(__dirname, "lib", "middleware.js"));
 const TurtleIO = require(path.join(__dirname, "lib", "turtleio.js"));
@@ -6,7 +7,7 @@ function factory () {
 	let app = new TurtleIO();
 
 	// Setting default middleware
-	[middleware.cors, middleware.etag, middleware.connect].forEach(function (i) {
+	array.each([middleware.cors, middleware.etag, middleware.connect], function (i) {
 		app.use(i).blacklist(i);
 	});
 
