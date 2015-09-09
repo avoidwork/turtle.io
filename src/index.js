96b9742ec2a5838c06aff9632d3186efad776c42
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-09-08 22:32:15 -0400

    Fixing partial `Buffer` / `String` responses

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const array = require("retsu");
 const path = require("path");
 const middleware = require(path.join(__dirname, "middleware.js"));
 const TurtleIO = require(path.join(__dirname, "turtleio.js"));
