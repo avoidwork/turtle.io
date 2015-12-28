dd0a46cd34f860cc2a7634997cf0ac09f7094add
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-12-28 09:56:32 -0500

    Updating sample & dep

--- sample.js
+++ sample.js
@@ -3,6 +3,10 @@
 var path = require("path"),
     server = require(path.join(__dirname, "lib", "index"))();
 
+server.get("/echo", function (req, res) {
+	res.send(req.parsed.query);
+});
+
 server.start({
 	default: "test",
 	root: path.join(__dirname, "sites"),
