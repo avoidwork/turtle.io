185b3228507aeed8fcd4b848ab1d8b4cb9ecb456
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-07-07 09:19:19 -0400

    Adding a logging hook for `304` responses which do not go through `respond()`

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ function factory (cfg = {}, errHandler = null) {
 		seed: obj.config.seed
 	});
 
+	// Making up for the ETag middleware
+	obj.router.onfinish = (req , res) => {
+		if (res.statusCode === 304) {
+			obj.log(obj.clf(req, res, res._headers), "info");
+		}
+	};
+
 	if (typeof errHandler === "function") {
 		obj.router.onerror = errHandler;
 	} else {
